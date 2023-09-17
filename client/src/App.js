14f6f1edbfa68ee44072adfd809a7f9fd8f9505d
anjanimenikdiwela98 <anjanimenikdiwela@gmail.com>
2023-09-17 14:02:36 +0530

    Updated all pages and add cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,40 @@
 import './App.scss';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import data from './data/items';
+import { useState } from 'react';
 
 function App() {
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
   return (
     <>
-      <Header />
-      <Home/>
+      <Header countCartItems={cartItems.length}></Header>
+      <Home products={products} onAdd={onAdd}></Home>
     </>
   );
 }
